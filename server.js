95dfc2229830b369b5e3fdc7721c1aba45d5a6fd
KJou <joukevin@gmail.com>
2019-03-11 19:39:59 -0700

    Refactor proxy routes for local testing

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const cors = require('cors');
 const compression = require('compression');
 const path = require('path');
 const favicon = require('serve-favicon')
+const proxy = require('http-proxy-middleware')
+
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
-app.use(compression());
+// app.use(compression());
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use('/api/cart/*', proxy({ target: 'http://localhost:3002', ws: true, changeOrigin: true}));
 
 app.use('/files', express.static(`${__dirname}/public`));
 
@@ -18,8 +22,12 @@ app.get('/products/:id', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.get('*', (req, res) => {
-  res.redirect('/products/1');
-})
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'public/index.html'));
+// });
 
-app.listen(80);
+// app.get('*', (req, res) => {
+//   res.redirect('/products/1');
+// })
+
+app.listen(3000);
