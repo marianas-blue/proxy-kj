d118bb46ac143a585174fe4bbccd651e3e27f90b
KJou <joukevin@gmail.com>
2019-03-12 15:48:57 -0700

    Refactor to use EC2

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const proxy = require('http-proxy-middleware')
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
-// app.use(compression());
+app.use(compression());
 
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-app.use('/api/cart/*', proxy({ target: 'http://localhost:3002', ws: true, changeOrigin: true}));
+app.use('/api/cart/*', proxy({ target: 'http://ec2-54-183-240-90.us-west-1.compute.amazonaws.com', ws: true, changeOrigin: true}));
 
 app.use('/files', express.static(`${__dirname}/public`));
 
@@ -22,12 +22,12 @@ app.get('/products/:id', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'public/index.html'));
-// });
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
-// app.get('*', (req, res) => {
-//   res.redirect('/products/1');
-// })
+app.get('*', (req, res) => {
+  res.redirect('/products/1');
+})
 
 app.listen(3000);
